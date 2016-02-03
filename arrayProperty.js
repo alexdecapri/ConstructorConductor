4866587be522cbfbae97af413a585e726b973085
alexdecapri <alexdecapri@gmail.com>
2016-02-02 19:30:37 -0700

    arrayProperty.js is finished

--- arrayProperty.js
+++ arrayProperty.js
@@ -5,7 +5,13 @@
 //Add a reverse method to the String 'class' so that every instance of 
 //String can call reverse and reverse itself.
   
-String.prototype.reverse = function(str) {
-	this.str = str;
-	return str.reverse();
-}
+String.prototype.reverse = function() {
+	return this.split("").reverse().join("");
+};
+
+// "String" class is default in JS
+
+// to test: var greeting = "Hello!";
+// greeting.reverse();
+
+//this.split takes string and splits characters into an array, then reverse reverses the order of an array
